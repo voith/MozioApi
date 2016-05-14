d1b21959c6e7c7b1e3c61a6e91a71445bd056bfe
voith <voithjm1@gmail.com>
2016-05-14 21:40:47 +0530

    added static_root

--- mozio/mozio/settings.py
+++ mozio/mozio/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles')
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
